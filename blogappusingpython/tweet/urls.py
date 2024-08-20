69516d6d5c486c23182fffab86aea89b64bfb235
devansh-kc <devansh.kc96@gmail.com>
2024-08-20 17:34:27 +0530

    worked on showing data

--- blogappusingpython/tweet/urls.py
+++ blogappusingpython/tweet/urls.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from . import views
 
 urlpatterns = [
-    path("",views.gtaIV, name="GtaIv"),
+    # path("",views.gtaIV, name="GtaIv"),
+    path("", views.tweet_list, name="tweet_list"),
+    path("create/", views.tweet_create, name="tweet_create"),
+    path("<int:tweet_id>/edit/", views.tweet_edit, name="tweet_edit"),
+    path("<int:tweet_id>/delete/", views.tweet_delete, name="tweet_delete"),
 ]
-     
-
-
