38f8c3750d821b05e436b1ab5652db1e09694dba
devansh-kc <devansh.kc96@gmail.com>
2024-08-21 17:20:30 +0530

    fixed typo

--- blogappusingpython/tweet/models.py
+++ blogappusingpython/tweet/models.py
@@ -5,12 +5,13 @@ from django.contrib.auth.models import User
 
 
 class Tweet(models.Model):
-    user=  models.ForeignKey(User,on_delete=models.CASCADE)
-    text= models.TextField(max_length = 300)
-    photo = models.ImageField( upload_to="photos/",null=True ,blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=300)
+    photo = models.ImageField(upload_to="photos/", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    
-    
+
+
 def __str__(self):
-    return f"{self.user.username}-{self.text[:10]}"
+    return self.user.username
+
