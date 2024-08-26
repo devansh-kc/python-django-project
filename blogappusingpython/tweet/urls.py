0713d87abc5cf84d009f6a5c5c59421cb5bc1f01
devansh-kc <devansh.kc96@gmail.com>
2024-08-26 21:35:14 +0530

    worked on pagination

--- blogappusingpython/tweet/urls.py
+++ blogappusingpython/tweet/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.shortcuts import render
-from .views import tweet_list, tweet_create, tweet_delete, tweet_edit, register
+from .views import tweet_list, tweet_create, tweet_delete, tweet_edit, register, gtaIV
 
 urlpatterns = [
     path("", tweet_list, name="tweet_list"),
+    path("gta_IV/", gtaIV, name="gta_IV"),
     path("create/", tweet_create, name="tweet_create"),
     path("<int:tweet_id>/edit/", tweet_edit, name="tweet_edit"),
     path("<int:tweet_id>/delete/", tweet_delete, name="tweet_delete"),
